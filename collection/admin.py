a34fc4ffb79cb291fe58933d5c5edc33adeca699
Adam <adam.amantea@petco.com>
2018-05-17 20:30:51 -0700

    Model and Admin Updates

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+# Import your model
+from collection.models import Concept
+
+# and setup automated slug creation
+class ConceptAdmin(admin.ModelAdmin):
+    model = Concept
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+# and register your model
+admin.site.register(Concept, ConceptAdmin)
