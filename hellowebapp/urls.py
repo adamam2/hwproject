6dedb9c694b6c7f25665c8f07de9b974da5bfa87
Adam <adam.amantea@petco.com>
2018-05-31 12:10:49 -0700

    Updates to login and registration with password reset

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -17,6 +17,14 @@ from django.urls import include, path
 from django.contrib import admin
 from django.views.generic import TemplateView
 from collection import views
+from django.contrib.auth.views import (
+    password_reset,
+    password_reset_done,
+    password_reset_confirm,
+    password_reset_complete,
+    password_change,
+    password_change_done,
+)
 
 urlpatterns = [
     path('', views.index, name='home'),
@@ -41,6 +49,30 @@ urlpatterns = [
          name='downloads'),
     path('concepts/<slug>/', views.concept_detail, name='concept_detail'),
     path('concepts/<slug>/edit/', views.edit_concept, name='edit_concept'),
+    
+    
     path('accounts/', include('registration.backends.simple.urls')),
+    
+    
+    path('accounts/password/reset/', password_reset,
+        {'template_name': 'registration/password_reset_form.html'},
+        name="password_reset"),
+    path('accounts/password/reset/done/', password_reset_done,
+        {'template_name': 'registration/password_reset_done.html'},
+        name="password_reset_done"),
+    path('accounts/password/reset/<uidb64>/<token>/', password_reset_confirm,
+        {'template_name': 'registration/password_reset_confirm.html'},
+        name="password_reset_confirm"),
+    path('accounts/password/rest/done/', password_reset_complete,
+        {'template_name': 'registration/password_reset_complete.html'},
+        name="password_reset_complete"),
+    
+    path('accounts/password/change/', password_change,
+        {'template_name': 'registration/password_change_form.html'},
+        name="password_change"),
+    path('accounts/password/change/done', password_change_done,
+         {'template_name': 'registration/password_change_done.html'},
+         name="password_change_done"),
+    
     path('admin/', admin.site.urls),
 ]
