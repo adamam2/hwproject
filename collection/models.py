dcec1b2e39a2841ec48e813b55c013287631dda5
Adam <adam.amantea@petco.com>
2018-06-12 11:39:03 -0700

    Updates to area model.

--- collection/models.py
+++ collection/models.py
@@ -6,15 +6,15 @@ class Concept(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     long_description = models.TextField(null=False, blank=True, default='', verbose_name='Long Description')
-    AREA_CHOICES = (('APP', 'Apps'),
-        ('DJG', 'Django'),
-        ('DOC', 'Docker'),
-        ('GEN', 'General'),
-        ('GIT', 'Git'),
-        ('HTML', 'HTML & CSS'),
-        ('JS', 'JavaScript'),
-        ('JQ', 'jQuery'),
-        ('PY','Python'))
+    AREA_CHOICES = (('Apps', 'Apps'),
+        ('Django', 'Django'),
+        ('Docker', 'Docker'),
+        ('General', 'General'),
+        ('Git', 'Git'),
+        ('HTML & CSS', 'HTML & CSS'),
+        ('JavaScript', 'JavaScript'),
+        ('jQuery', 'jQuery'),
+        ('Python','Python'))
     area = models.CharField(max_length=255, choices=AREA_CHOICES, verbose_name='Area', default='APP')
     example = models.FileField(null=False, blank=True, default='')
     slug = models.SlugField(unique=True)
